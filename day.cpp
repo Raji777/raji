813b83cdbe09dd95c9db1d1a6700ddba6ebf9ffb
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-03-02 09:35:10 +0530

    day.cpp

---
+++ day.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+char s[20];
+cout<<"Enter the day";
+cin>>s;
+switch(s)
+{
+case monday:
+cout<<"no";
+break;
+case tuesday:
+cout<<"no";
+break;
+case wednesday:
+cout<<"no";
+break;
+case thursday:
+cout<<"no";
+break;
+case friday:
+cout<<"no";
+break;
+case saturday:
+cout<<"yes";
+break;
+case sunday:
+cout<<"yes";
+break;
+}
+	return 0;
+}
