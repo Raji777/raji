1d4dcd88c217d28509cc8562a0dc91cf1f2f83a1
Raji777 <36372373+Raji777@users.noreply.github.com>
2018-03-02 09:06:33 +0530

    character.cpp

---
+++ character.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#include <string.h>
+
+using namespace std;
+
+
+int main()
+ 
+{
+
+int n;
+
+char s1[n],s2[n];
+
+cout<<"enter two strings"<<endl;
+
+cin>>s1>>s2;
+
+if(strlen(s1),strlen(s2))
+
+{
+	
+for(int i=0;i<=n;i++)
+	
+{
+	
+if(s1[i]==s2[i])
+	
+{
+		
+cout<<"the strings are same";
+
+}
+	
+else 
+	
+cout<<"the strings are differ by character";
+
+}
+
+}
+
+else 
+cout<<"strings are different ";
+	
+return 0;
+
+}
